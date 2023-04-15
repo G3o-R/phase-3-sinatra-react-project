610b3f0443f421d0dc89190aa71569cef98b97b2
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-04-15 16:32:14 -0700

    API includes jobs with application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < Sinatra::Base
   # Add your routes here
   get "/applications" do
     application = Application.all
-    application.to_json
+    application.to_json(include: :jobs)
     # { message: "Good luck with your project!" }.to_json
+  end
+  
+  get "/applications/:id" do
+    application = Application.find(params[:id])
+    application.to_json
   end
 
 end

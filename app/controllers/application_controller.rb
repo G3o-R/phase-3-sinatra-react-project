ddcf5a3857d9ed17d7f27adb67149bdcc567464f
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-04-17 18:33:04 -0700

    made post route for company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  
+  get "/" do
+    "its all working".to_json
+  end
 
   get "/companies" do
     companies = Company.all
     companies.to_json(include:{ jobs: {include: :applicants}})
+  end
+
+  post "/company" do
+    company = Company.create(
+      company_name: params[:company_name],
+    )
+    Company.to_json
   end
 
 end
